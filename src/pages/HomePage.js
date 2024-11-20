a8e1387b0f1111e9806f50a70c2fdde3c7b083c9
ethan m <48036998+EMacson@users.noreply.github.com>
2024-11-19 21:07:06 -0800

    home page set up

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Rain Visualizer</h1>
+      <p>A website for visualizing how much rain will fall per hour or day.</p>
+    </div>
+  );
+};
+
+export default HomePage;
